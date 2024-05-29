489e57ef9d78a5482015674960c388030ab8b584
Srishtigoel1 <112386808+Srishtigoel1@users.noreply.github.com>
2024-05-29 17:58:03 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,24 @@
+import random 
+#generating random password for suggestions
+uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase=uppercase.lower()
+digits="0123456789"
+symbols="!@#$%^&*()_+{|[]\-><,.?/}~`"
+upper,lower,nums,syms=True,True,True,True
+
+all=""
+if upper:
+    all+=uppercase
+if lower:
+    all+=lowercase
+if nums:
+    all+=digits
+if syms:
+    all+=symbols
+#print(all)
+length=20 #length of the password 
+amount=10 #Number of password to generate
+for x in range(amount):
+    password="".join(random.sample(all,length))
+    print(password)
+    print("\n")
